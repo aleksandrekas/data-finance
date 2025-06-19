626f8f003f56780a828a394858683c094b149452
aleksandre <aleksandrekasrashvili@gmail.com>
2025-06-19 22:52:19 +0400

    added homepage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,37 @@
+export default function HomePage() {
+  const buttonStyle = 'h-[40px] rounded-[5px] text-[0.8rem] cursor-pointer';
+  const green = '#0c7958';
+
+  return (
+    <div id="container" className="w-full h-screen bg-black">
+      <nav className="flex justify-between w-[60%] mx-auto pt-6">
+        <div className="text-[#0c7958] font-bold text-3xl">REACT.</div>
+        <div className="flex">
+          <ul className="flex flex-row justify-center items-center mr-2">
+            <ListItem text={"Home"} />
+            <ListItem text={"Account"} />
+            <ListItem text={"Sign in"} />
+          </ul>
+          <button className={`${buttonStyle} w-[100px] bg-white hover:bg-[#0c7958] hover:text-white`}>
+            Get Started
+          </button>
+        </div>
+      </nav>
+
+      <section id="homeSection" className="flex flex-col justify-center items-center mt-40">
+        <h5 className="text-[#0c7958] font-bold mb-4">GROWING WITH DATA ANALYTICS</h5>
+        <h1 className="text-white text-6xl font-medium mb-4">Grow with data.</h1>
+        <h2 className="text-white font-medium text-5xl mb-4">Fast, flexible financing for ...</h2>
+        <p className="mb-4 w-[800px] text-center text-[#8d918e] font-bold text-2xl">Monitor your data analytics to increase revenue for B2B, BTC, & SaaS platforms</p>
+        <button className="bg-[#0c7958] text-black px-4 py-2 rounded-[5px]">Get Started</button>
+      </section>
+    </div>
+  );
+}
+
+
+function ListItem({text}){
+    return (
+        <li className="text-white mx-1.5 hover:text-[#0c7958] cursor-pointer">{text}</li>
+    )
+}
